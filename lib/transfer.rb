c9d5024d2e804940a0d8f3b22452a6beddc654a1
Anthony George <ageorge530@gmail.com>
2020-06-11 04:51:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,10 +24,10 @@
   end
  
    def reverse_transfer
-     if valid? && receiver.balance > amount && self.status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
-      self.status = "complete"
+     if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
     else
       reject_transfer
     end
