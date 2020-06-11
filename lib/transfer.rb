4451ac317d101b6872d2b24169928d3ca92562b0
Anthony George <ageorge530@gmail.com>
2020-06-11 04:47:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,17 +12,15 @@
   def valid?
     sender.valid? && receiver.valid?
   end
-  
-  def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-    sender.balance -= amount
-    receiver.balance += amount
-    self.status = "complete"
-  else
-   reject_transfer
-  end
- end 
  
-   
+   def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
  
 end 
